be94db08923d00706f2d8edf2e587baad689e946
Daniel Maestro Gomez <dmaestro@c2r12s1.42madrid.com>
2026-02-28 16:46:31 +0100

    a

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmaestro <dmaestro@student.42madrid.con    +#+  +:+       +#+        */
+/*   By: dmaestro <dmaestro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 12:41:13 by dmaestro          #+#    #+#             */
-/*   Updated: 2026/02/28 15:39:51 by dmaestro         ###   ########.fr       */
+/*   Updated: 2026/02/28 15:50:17 by dmaestro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int main()
 {
     Burocrats paco("paco", 3);
+    Burocrats pepe(paco);
     Burocrats francisco("francisco", 120);
 
     try{
